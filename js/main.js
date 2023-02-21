69eb9ab36f3bec771a4219a369b6c0f56fb9e1d3
Doctor Hawk <gnimassouothniel@gmail.com>
2023-02-21 21:35:34 +0000

    depot de fichier

---
+++ js/main.js
@@ -0,0 +1,27 @@
+// changer la couleur de la NAVBAR quand user defile la page
+
+window.addEventListener('scroll', () => {
+    document.querySelector('nav').classList.toggle
+    ('window-scroll', window.scrollY > 0)
+})
+
+// MENU HAMBERGEUR
+const menu = document.querySelector(".nav__menu");
+const menuBtn = document.querySelector("#open-menu-btn");
+const closeBtn = document.querySelector("#close-menu-btn");
+
+
+menuBtn.addEventListener('click', () => {
+    menu.style.display = "flex";
+    closeBtn.style.display = "inline-block";
+    menuBtn.style.display = "none";
+})
+
+// FERMER LE MENU HAMBURGER
+const closeNav = () => {
+    menu.style.display = "none";
+    closeBtn.style.display = "none";
+    menuBtn.style.display = "inline-block";
+}
+
+closeBtn.addEventListener('click',  closeNav)
